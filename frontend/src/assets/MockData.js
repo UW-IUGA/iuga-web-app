8aa5180a95a7660c50ef67eb67b9177dfdf54a57
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2024-04-24 19:19:33 -0700

    fix mock data

--- frontend/src/assets/MockData.js
+++ frontend/src/assets/MockData.js
@@ -3,24 +3,28 @@ export const mockEvents = [
         "eName": "Networking + Industry Panel",
         "eOrganizers": "UX@UW and HCDEsa",
         "eDescription": "Eager to learn more about how to get your foot in the door with big tech companies fo...",
-        "eLabels": ["Social", "Career"]
+        "eLabels": ["Social", "Career"],
+        "eStartDate": "2024-04-05"
     },
     {
         "eName": "Hack for Social Good 2024",
         "eOrganizers": "IUGA, Winfo, binfo",
         "eDescription": "Eager to learn more about how to get your foot in the door with big tech companies fo...",
-        "eLabels": ["Social", "Academic", "Career"]
+        "eLabels": ["Social", "Academic", "Career"],
+        "eStartDate": "2024-04-05"
     },
     {
         "eName": "Coffee & Coloring",
         "eOrganizers": "IUGA",
         "eDescription": "Eager to learn more about how to get your foot in the door with big tech companies fo...",
-        "eLabels": ["Social"]
+        "eLabels": ["Social"],
+        "eStartDate": "2024-04-05"
     },
     {
         "eName": "Networking + Industry Panel",
         "eOrganizers": "UX@UW and HCDEsa",
         "eDescription": "Eager to learn more about how to get your foot in the door with big tech companies fo...",
-        "eLabels": ["Academic", "Social", "Career"]
+        "eLabels": ["Academic", "Social", "Career"],
+        "eStartDate": "2024-04-05"
     }
 ];

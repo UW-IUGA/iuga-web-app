ac997be56c45a7badfc4238b0281b1376afccc54
VERAGUBER <VERA2@UW.EDU>
2024-04-24 22:45:02 -0700

    Started working on basic about card

---
+++ frontend/src/components/AboutCard.jsx
@@ -0,0 +1,26 @@
+const aboutCard =
+    (/*{ officerImgSrc, officerName, officerQuote, responsibilityList }*/) => {
+        return (
+            <div className="aboutCard">
+                <img src="" alt="" />
+                <div>
+                    <h2>Member Name</h2>
+                    <div>
+                        <h3>Member Name</h3>
+                        <p>
+                            “IUGA has taught me so much about leadership and
+                            community building at UW. I am excited to meet and
+                            connect with more Informatics students!”
+                        </p>
+                    </div>
+                    <ul>
+                        <li>responsibility 1</li>
+                        <li>responsibility 2</li>
+                        <li>responsibility 3</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    };
+
+export default aboutCard;

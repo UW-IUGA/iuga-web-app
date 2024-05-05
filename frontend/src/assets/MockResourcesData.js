e21d636132992d0532f2ff0b930807e90a8a05f9
VERAGUBER <VERA2@UW.EDU>
2024-05-04 22:49:53 -0700

    Work in progress; resource page

---
+++ frontend/src/assets/MockResourcesData.js
@@ -0,0 +1,52 @@
+export const mockResources = [
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "academic",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "academic",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "career",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "career",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "community",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "community",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "mental health",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "mental health",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "dei",
+  },
+  {
+    "rName": "Informatics Undergraduate Association",
+    "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+    "tag": "dei",
+  },
+]
--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -3,6 +3,7 @@ function ResourcesPage() {
         <div className="baseContainer">
             <div className="resourceSum">
                 <img
+                    className="resourceImg"
                     src="/assets/resources.png"
                     alt="student looking for resources"
                 />
@@ -15,6 +16,25 @@ function ResourcesPage() {
                         fun, community building events as well as events for
                         professional development.
                     </p>
+                </div>
+            </div>
+            <div className="resourceContainer">
+                <div className="legend">
+                    <h3>Categories</h3>
+                    <ul>
+                        <li>Academic</li>
+                        <li>Career</li>
+                        <li>Community</li>
+                        <li>Mental Health</li>
+                        <li>DEI</li>
+                    </ul>
+                </div>
+                <div className="resources">
+                    <h2>Academic</h2>
+                    <h2>Career</h2>
+                    <h2>Community</h2>
+                    <h2>Mental Health</h2>
+                    <h2>Diversity, Equity, and Inclusion (DEI)</h2>
                 </div>
             </div>
         </div>

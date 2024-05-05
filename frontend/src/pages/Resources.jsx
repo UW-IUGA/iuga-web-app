46151ab7acfa2da54a9f9c8378823924d2cabdfc
VERAGUBER <VERA2@UW.EDU>
2024-05-04 17:43:13 -0700

    started resource page styling

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -1,10 +1,24 @@
 function ResourcesPage() {
     return (
         <div className="baseContainer">
-            <h1>Resources</h1>
+            <div className="resourceSum">
+                <img
+                    src="/assets/resources.png"
+                    alt="student looking for resources"
+                />
+                <div className="summary">
+                    <h1>Resources</h1>
+                    <p>
+                        IUGA is a student-led RSO that communicates between the
+                        Informatics student body, faculty, and administration of
+                        the University of Washington Information School. We host
+                        fun, community building events as well as events for
+                        professional development.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
-  
+
 export default ResourcesPage;
-  

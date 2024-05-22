4f1ffb6f078dd0813bb81f6089bd55b09b4bc675
VERAGUBER <VERA2@UW.EDU>
2024-05-21 20:37:18 -0700

    Changing calendar 'selected' states

--- frontend/src/assets/mock-data/Enum.js
+++ frontend/src/assets/mock-data/Enum.js
@@ -7,5 +7,24 @@ export const resourceTags = {
 }
 
 export const aboutTags = {
-  RSO: "rso",
+  RSO: "rso"
+}
+
+export const groupType = {
+  OFFICERS: "officers",
+  WEB_COMMITTEE: "web committee",
+  CREATIVE_COMMITTEE: "creative committee"
+}
+
+export const officerPositions = {
+  PRESIDENT: "President",
+  COPRESIDENT: "Co-President",
+  VICE_PRESIDENT: "Vice President",
+  FINANCE: "Director of Finance",
+  CREATIVE: "Creative Director",
+  PUBLIC_RELATIONS: "Director of Public Relations",
+  OUTREACH: "Director of Outreach",
+  DIVERSITY: "Director of Diversity Efforts",
+  IT: "Director of Information Technology",
+  ACADEMIC: "Director of Academic Support"
 }
--- frontend/src/assets/mock-data/MockAboutData.js
+++ frontend/src/assets/mock-data/MockAboutData.js
@@ -1,16 +1,51 @@
-import { aboutTags } from './Enum';
+import { aboutTags } from "./Enum";
+import { groupType } from "./Enum";
+import { officerPositions } from "./Enum";
 
 export const mockMembers = {
-  "members": [],
-  "rsos": [{
-      "rName": "Informatics Undergraduate Association",
-      "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
-      "tag": aboutTags.RSO,
+    members: {
+        2024: {
+            [groupType.BOARD_MEMBERS]: [
+                {
+                    position: officerPositions.COPRESIDENT,
+                    name: "Sirena Akopyans",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/sirena-akopyan/",
+                },
+                {
+                    position: officerPositions.COPRESIDENT,
+                    name: "Phyllis Chen",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/phyllis-chen-profile/",
+                },
+            ],
+            [groupType.WEB_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+            [groupType.CREATIVE_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+        },
+        2023: {
+            // ...
+        },
     },
-    {
-      "rName": "Informatics Undergraduate Association",
-      "rDescription": "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
-      "tag": aboutTags.RSO,
-    }
-  ]
+    rsos: [
+        {
+            rName: "Informatics Undergraduate Association",
+            rDescription:
+                "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+            tag: aboutTags.RSO,
+        },
+        {
+            rName: "Informatics Undergraduate Association",
+            rDescription:
+                "IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and administration of the University of Washington Information School. We host fun, community building events as well as events for professional development.",
+            tag: aboutTags.RSO,
+        },
+    ],
 };

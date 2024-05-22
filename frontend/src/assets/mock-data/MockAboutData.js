b261fc73649a6801bc2dd63f0bac6e7f8ffcdc2b
VERAGUBER <VERA2@UW.EDU>
2024-05-22 00:20:56 -0700

    about data

--- frontend/src/assets/mock-data/MockAboutData.js
+++ frontend/src/assets/mock-data/MockAboutData.js
@@ -18,6 +18,48 @@ export const mockMembers = {
                     picture: "",
                     linkedin: "https://www.linkedin.com/in/phyllis-chen-profile/",
                 },
+                {
+                    position: officerPositions.FINANCE,
+                    name: "Brandon Mendoza",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/bwmendo/",
+                },
+                {
+                    position: officerPositions.CREATIVE,
+                    name: "Sheamin Kim",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/sheamin-kim-b21818250/",
+                },
+                {
+                    position: officerPositions.PUBLIC_RELATIONS,
+                    name: "Ryan Louie",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/rylouie/",
+                },
+                {
+                    position: officerPositions.OUTREACH,
+                    name: "Suraj Gangaram",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/suraj-gangaram/",
+                },
+                {
+                    position: officerPositions.DIVERSITY,
+                    name: "Mari Woodworth",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/marikowoodworth/",
+                },
+                {
+                    position: officerPositions.IT,
+                    name: "Eric Kim",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/taehyunnkim/",
+                },
+                {
+                    position: officerPositions.ACADEMIC,
+                    name: "Bhavya Garlapati",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/bhavya-garlapati-95ab46225/",
+                },
             ],
             [groupType.WEB_COMMITTEE]: [
                 {
@@ -30,8 +72,343 @@ export const mockMembers = {
                 },
             ],
         },
-        2023: {
-            // ...
+        2020: {
+            [groupType.BOARD_MEMBERS]: [
+                {
+                    position: officerPositions.PRESIDENT,
+                    name: "Kiran Pradan",
+                    picture: "",
+                    github: "https://github.com/kiranpradhan01",
+                    linkedin: "https://www.linkedin.com/in/kiran-pradhan/",
+                },
+                {
+                    position: officerPositions.VICE_PRESIDENT,
+                    name: "Jacinda Eng",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/jacinda-eng-6432a8171",
+                    github: "https://github.com/jacindaeng",
+                    website: "https://jacindaeng.com/",
+                },
+                {
+                    position: officerPositions.FINANCE,
+                    name: "Alexander Sanchez",
+                    picture: "",
+                    github: "https://github.com/AlexanderES",
+                    linkedin: "https://www.linkedin.com/in/alexander-escalera-503360176/",
+                },
+                {
+                    position: officerPositions.CREATIVE,
+                    name: "Jojo Saunders",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/jojosaunders/",
+                    website: "https://www.jojosaunders.me/",
+                },
+                {
+                    position: officerPositions.PUBLIC_RELATIONS,
+                    name: "Cheryl Wu",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/cheryl-wu-59baa8174",
+                    website: "https://wucheryl.com",
+                },
+                {
+                    position: officerPositions.OUTREACH,
+                    name: "Ana De Las Alas",
+                    picture: "",
+                    github: "https://github.com/dayoungcheong",
+                    linkedin: "https://www.linkedin.com/in/ana-de-las-alas/",
+                },
+                {
+                    position: officerPositions.DIVERSITY,
+                    name: "Aaron Zhao",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/zhaoaaron/",
+                    website: "https://www.aaronzhao.io/",
+                },
+                {
+                    position: officerPositions.IT,
+                    name: "Andrey Butenko",
+                    picture: "",
+                    github: "https://github.com/andreybutenko",
+                    linkedin: "https://www.linkedin.com/in/butenkoandrey/",
+                    website: "https://andreybutenko.com/",
+                },
+                {
+                    position: officerPositions.ACADEMIC,
+                    name: "Joseph Altamira",
+                    picture: "",
+                    github: "https://github.com/Jornalt",
+                    linkedin: "https://www.linkedin.com/in/joseph-altamira-0b0a22161/",
+                },
+            ],
+            [groupType.WEB_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+            [groupType.CREATIVE_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+        },
+        2019: {
+            [groupType.BOARD_MEMBERS]: [
+                {
+                    position: officerPositions.PRESIDENT,
+                    name: "Alejandro Huante",
+                    picture: "",
+                    github: "https://github.com/ahuante-1741170",
+                    linkedin: "https://www.linkedin.com/in/alejandro-huante-28533a141/",
+                },
+                {
+                    position: officerPositions.VICE_PRESIDENT,
+                    name: "James Kim",
+                    picture: "",
+                    github: "https://github.com/thejameskim",
+                    linkedin: "https://www.linkedin.com/in/thejameskim/",
+                },
+                {
+                    position: officerPositions.FINANCE,
+                    name: "Kiran Pradhan",
+                    picture: "",
+                    github: "https://github.com/kiranpradhan01",
+                    linkedin: "https://www.linkedin.com/in/kiran-pradhan-aa8861162/",
+                },
+                {
+                    position: officerPositions.CREATIVE,
+                    name: "Casey Tran",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/caseytran/",
+                },
+                {
+                    position: officerPositions.PUBLIC_RELATIONS,
+                    name: "Allison Lee",
+                    picture: "",
+                    github: "https://github.com/alliL",
+                    linkedin: "https://www.linkedin.com/in/allison20/",
+                },
+                {
+                    position: officerPositions.OUTREACH,
+                    name: "Dayoung Cheong",
+                    picture: "",
+                    github: "https://github.com/dayoungcheong",
+                    linkedin: "https://www.linkedin.com/in/dayoungcheong/",
+                },
+                {
+                    position: officerPositions.IT,
+                    name: "William Kwok",
+                    picture: "",
+                    github: "https://github.com/kwokwilliam",
+                    linkedin: "https://www.linkedin.com/in/william-w-kwok/",
+                    website: "https://williamk.info",
+                },
+                {
+                    position: officerPositions.ACADEMIC,
+                    name: "Shruti Rajagopalan",
+                    picture: "",
+                    github: "https://github.com/ShrutiR5",
+                    linkedin: "https://www.linkedin.com/in/shrutira/",
+                },
+            ],
+            [groupType.WEB_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+            [groupType.CREATIVE_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+        },
+        2018: {
+            [groupType.BOARD_MEMBERS]: [
+                {
+                    position: officerPositions.PRESIDENT,
+                    name: "Andrea Chen",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/abchen",
+                    website: "http://abchen.com/",
+                    github: "https://github.com/andyblueyo",
+                },
+                {
+                    position: officerPositions.VICE_PRESIDENT,
+                    name: "Manó Bárkovics",
+                    picture: "",
+                    website: "http://manobarkovics.com/",
+                    linkedin: "https://www.linkedin.com/in/manobarkovics",
+                },
+                {
+                    position: officerPositions.FINANCE,
+                    name: "Anton Zheng",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/antonzheng",
+                },
+                {
+                    position: officerPositions.CREATIVE,
+                    name: "Oorja Chowdhary",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/oorjac",
+                },
+                {
+                    position: officerPositions.PUBLIC_RELATIONS,
+                    name: "Neha Yadav",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/neha-yadav-2b6498109",
+                },
+                {
+                    position: officerPositions.OUTREACH,
+                    name: "Cole French",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/colemanfrench",
+                },
+                {
+                    position: officerPositions.IT,
+                    name: "Kidus Sendeke",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/kidus-sendeke",
+                },
+                {
+                    position: officerPositions.DIVERSITY,
+                    name: "Katie Goulding",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/kate-goulding-613569164",
+                },
+                {
+                    position: officerPositions.ACADEMIC,
+                    name: "Joseph Tsai",
+                    picture: "",
+                    linkedin: "https://www.linkedin.com/in/josephktsai",
+                },
+            ],
+            [groupType.WEB_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+            [groupType.CREATIVE_COMMITTEE]: [
+                {
+                    name: "",
+                },
+            ],
+        },
+    },
+    positionInformation: {
+        [officerPositions.PRESIDENT]: {
+            description: "The President is the head of IUGA and their responsibilities include:",
+            responsibilities: [
+                "Lead IUGA Meetings",
+                "Represent Informatics to faculty and staff with the Vice President",
+                "Shepherd and guide newly elected IUGA Officers during the annual transition",
+                "Act as backup for the Director of Finance for financial matters",
+                "Act as, or designate, the Informatics Representative On the iSchool Program Committee",
+                "Act as liaison to other iSchool student organizations",
+            ],
+        },
+        [officerPositions.COPRESIDENT]: {
+            description: "The Co-Presidents are the joint heads of IUGA and their responsibilities include:",
+            responsibilities: [
+                "Co-lead IUGA Meetings",
+                "Assist each other in representing Informatics to faculty and staff",
+                "Shepherd and guide newly elected IUGA Officers during the annual transition",
+                "Act as backup for the Director of Finance for financial matters",
+                "Act as, or designate, the Informatics Representative On the iSchool Program Committee",
+                "Act as liaison to other iSchool student organizations",
+            ],
+        },
+        [officerPositions.VICE_PRESIDENT]: {
+            description:
+                "The Vice President fills roles as needed and has core repsonsibilities with which they are entrusted. These core responsibilities include the following:",
+            responsibilities: [
+                "Backup for the President",
+                "Collaborates with the Director of Outreach when communicating with External Groups",
+                "Responsible for scheduling bi-weekly meetings",
+                "Responsible for booking venues for all IUGA events",
+                "Responsible for handling the yearly Registration of IUGA with the SAO",
+                "Responsible for the meeting minutes of every IUGA meeting",
+                "Act as the Primary Point of Contact for Student Services when dealing with the Informatics Recruitment Process",
+                "Fulfill or Designate a student to fill a seat on the iSchool Student Leadership Committee and the Informatics Program Committee",
+            ],
+        },
+        [officerPositions.FINANCE]: {
+            description:
+                "The director of finance maintains the Association’s financial records. This includes the following items:",
+            responsibilities: [
+                "Maintain the Financial Records",
+                "Provide Reports On Finances to IUGA",
+                "Fulfill or Designate a Student Representative for Informatics on the iSchool Student Leadership Committee",
+                "Organize the Financial Vectors of IUGA fundraising efforts",
+                "Prepare and Submit the Yearly IUGA Budget to iSchool Administration",
+                "Act as the Primary Account Holder for All IUGA Financial Accounts",
+                "Handle merchandise ordering and distribution",
+                "Help the incoming Director of Finance with the Yearly Budget during Spring Quarter",
+            ],
+        },
+        [officerPositions.CREATIVE]: {
+            description:
+                "The Creative Director will work on creating the visual style of the IUGA brand and take initiative in creating and delivering branded items. These responsibilities will include:",
+            responsibilities: [
+                "Creating merchandise for IUGA, including clothing",
+                "Creating informational posters/flyers for IUGA events",
+                "Maintain the IUGA logo and design styling",
+                "Create a team to help in the creation of Informatics branded items",
+                "Assist Finance with ordering and distribution of merchandise",
+            ],
+        },
+        [officerPositions.PUBLIC_RELATIONS]: {
+            description:
+                "The Director of Public Relations serves a vital communication role within the Executive Council. These responsibilities Include:",
+            responsibilities: [
+                "Communicate Announcement and Events to Informatics Students",
+                "Lead the Planning and Organization of Social Events",
+                "Ensure the Smooth Operation and Execution of All IUGA events",
+                "Oversee Elections",
+            ],
+        },
+        [officerPositions.OUTREACH]: {
+            description:
+                "The Director of Outreach is responsible for representing both the Informatics Program and the IUGA to the professional community. They do this by:",
+            responsibilities: [
+                "Working with Students Services and the Career Center to promote employment and professional development events.",
+                "Work with Student Services to facilitate alumni relations",
+                "Promote Informatics to the University of Washington at Large and the Professional Community Along with the Vice President",
+                "Responsible for organizing and executing professional development events for Informatics",
+            ],
+        },
+        [officerPositions.DIVERSITY]: {
+            description:
+                "The Director of Diversity Efforts works to promote diversity both within the major and the iSchool. This includes, but is not limited to, the following:",
+            responsibilities: [
+                "Support and promote activist-oriented RSOs",
+                "Organize and promote diversity and inclusion events for the iSchool",
+                "Work closely with the Diversity Programs Advisor to ideate and execute diversity initiatives",
+                "Support and engage in an active Diversity Committee composed of students, faculty and staff",
+                "Fulfill, or appoint a student, to sit on the Informatics Admissions Committee during the summer",
+                "Ensure that the events, ideas, and products of IUGA are made with the thought of diversity and inclusion",
+                "Collaborate with Student Services and other Registered Student Organizations (RSOs) on campus for diversity and inclusion events",
+            ],
+        },
+        [officerPositions.IT]: {
+            description:
+                "The Director of Information Technology helps run the Association’s technology projects and online presence. While providing support when needed. This is done by doing the following:",
+            responsibilities: [
+                "Maintain and update the IUGA Website and associated websites using the latest web technologies",
+                "Monitor and maintain the IUGA gaming servers and associated assets",
+                "Maintain all IUGA online account credentials in a secure and scalable system",
+                "Manage permissions and security settings for IUGA assets across social networks and collaborative systems",
+                "Oversee and run technology aspects of IUGA events, including the quarterly Game Night",
+                "Work closely with iSchool IT and the Administration to connect the student body with all available technology and services",
+                "Support, Service, and Create Technology solutions for needs within the Association and the iSchool",
+            ],
+        },
+        [officerPositions.ACADEMIC]: {
+            description:
+                "The Director of Academic Support is responsible for supporting the academic needs within the Informatics Program. These responsibilities include:",
+            responsibilities: [
+                "Understand the needs of current students",
+                "Organize and promote academic related events",
+                "Work closely with other organizations to conduct joint events",
+            ],
         },
     },
     rsos: [

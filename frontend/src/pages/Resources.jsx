1b2d8d004365af3bf4fd3d540b90dadea741fb70
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2024-05-23 13:25:51 -0700

    update resources page layout

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -25,57 +25,61 @@ function ResourcesPage({ resources }) {
 
     return (
         <div className="baseContainer">
-            <div className="resourceSum">
+            <div className="resourceContainer">
                 <div className="blob1"></div>
                 <div className="blob2"></div>
-                <img className="resourceImg" src="/assets/resources-main.png" alt="student looking for resources" />
-                <div className="summary">
-                    <h1>Resources</h1>
-                    <p>
-                        IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and
-                        administration of the University of Washington Information School. We host fun, community
-                        building events as well as events for professional development.
-                    </p>
+                <div className="blob3"></div>
+                <div className="resourceHeader">
+                    <img className="resourceImg" src="/assets/resources-main.png" alt="student looking for resources" />
+                    <div className="resourceSummary">
+                        <h1>Resources</h1>
+                        <p>
+                            IUGA is a student-led RSO that communicates between the Informatics student body, faculty, and
+                            administration of the University of Washington Information School. We host fun, community
+                            building events as well as events for professional development.
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className="resourceContainer">
-                <div className="legend">
-                    <div className="blob3"></div>
-                    <h3 className="legendHeader">Categories</h3>
-                    <ul>
-                        {categories.map((category) => (
-                            <li key={category} onClick={() => handleClick(category)}>
-                                {category === "Diversity, Equity, and Inclusion (DEI)" ? "DEI" : category}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="resources">
-                    {categories.map((category) => (
-                        <div key={category} ref={(el) => (categoryRefs.current[category] = el)}>
-                            {category === "Diversity, Equity, and Inclusion (DEI)" ? (
-                                <>
-                                    <h2>Diversity, Equity, and Inclusion (DEI)</h2>
-                                    <GradientLine />
-                                    {resources
-                                        .filter((resource) => resource.tag === "dei")
-                                        .map((filteredResource) => (
-                                            <ResourceCard key={filteredResource.rName} resource={filteredResource} />
-                                        ))}
-                                </>
-                            ) : (
-                                <>
-                                    <h2>{category}</h2>
-                                    <GradientLine />
-                                    {resources
-                                        .filter((resource) => resource.tag === category.toLowerCase())
-                                        .map((filteredResource) => (
-                                            <ResourceCard key={filteredResource.rName} resource={filteredResource} />
-                                        ))}
-                                </>
-                            )}
+                <div className="resourceContentContainer">
+                    <div className="legendContainer">
+                        <div className="legend">
+                            <h3 className="legendHeader">Categories</h3>
+                            <ul>
+                                {categories.map((category) => (
+                                    <li key={category} onClick={() => handleClick(category)}>
+                                        {category === "Diversity, Equity, and Inclusion (DEI)" ? "DEI" : category}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
-                    ))}
+                    </div>
+                    <div className="resourcesWrapper">
+                        {categories.map((category) => (
+                            <div key={category} ref={(el) => (categoryRefs.current[category] = el)}>
+                                {category === "Diversity, Equity, and Inclusion (DEI)" ? (
+                                    <>
+                                        <h2>Diversity, Equity, and Inclusion (DEI)</h2>
+                                        <GradientLine className="fullWidth" />
+                                        {resources
+                                            .filter((resource) => resource.tag === "dei")
+                                            .map((filteredResource) => (
+                                                <ResourceCard key={filteredResource.rName} resource={filteredResource} />
+                                            ))}
+                                    </>
+                                ) : (
+                                    <>
+                                        <h2>{category}</h2>
+                                        <GradientLine className="fullWidth" />
+                                        {resources
+                                            .filter((resource) => resource.tag === category.toLowerCase())
+                                            .map((filteredResource) => (
+                                                <ResourceCard key={filteredResource.rName} resource={filteredResource} />
+                                            ))}
+                                    </>
+                                )}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>

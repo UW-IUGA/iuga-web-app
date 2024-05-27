6d553c2650cb0a1d80efd967f9832612c53bdfc3
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2024-05-27 13:40:42 -0700

    update mongodb connection

--- backend/models.js
+++ backend/models.js
@@ -7,15 +7,25 @@ import {
 
 let models = {};
 
+const prod_account = {
+    user: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+}
+
+const dev_account = {
+    user: process.env.DB_DEV_USERNAME,
+    password: process.env.DB_DEV_PASSWORD,
+}
+
 async function connectToDatabase(){
     console.log('connecting to mongodb')
     if (process.env.DEPLOY) {
-        console.log(process.env.TEST_ENV_VAR)
-        // Temporary
-        await mongoose.connect('mongodb+srv://iuga:78B5aJunY5ZrypC1@cluster0.ejo8heu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-        console.log("successfully connected to deployment mongodb")
+        const prod_uri = `mongodb://${prod_account.user}:${prod_account.password}@mongo:27017/iuga`;
+        await mongoose.connect(prod_uri);
+        console.log("successfully connected to prod mongodb")
     } else {
-        await mongoose.connect('mongodb+srv://iuga:78B5aJunY5ZrypC1@cluster0.ejo8heu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        const dev_uri = `mongodb+srv://${dev_account.user}:${dev_account.password}@cluster0.ejo8heu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+        await mongoose.connect(dev_uri);
         console.log("successfully connected to dev mongodb")
     }
     

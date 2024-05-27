43a25f1c5ee078a414544cb38bfccc0b8ec2588f
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2024-05-27 04:05:13 -0700

    add ga

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,13 +6,14 @@ import { msalConfig } from './authConfig'
 import { MsalProvider } from '@azure/msal-react' 
 import { PublicClientApplication } from '@azure/msal-browser'
 import { AuthProvider } from './context/AuthContext';
+import ReactGA from "react-ga4";
 import './stylesheets/main.scss';
 
 const msalInstance = new PublicClientApplication(msalConfig);
 await msalInstance.initialize();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+ReactGA.initialize("G-S0M9Y5V0XT");
 root.render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>

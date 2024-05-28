28d784c73a1fd7cd8f4877cb3b32a8ceff0dcfda
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2024-05-28 11:53:55 -0700

    add 2022 team socials

--- frontend/src/assets/data/teams/2022.js
+++ frontend/src/assets/data/teams/2022.js
@@ -20,60 +20,78 @@ export const team_2022 = {
 			position: officerPositions.COPRESIDENT,
             name: "Justin Zeng",
             picture: _justin,
-            github: 'https://github.com/Once28',
-			linkedin: "https://www.linkedin.com/in/justin-zeng-3b7b47165/"
+			socials: {
+				github: 'https://github.com/Once28',
+				linkedin: "https://www.linkedin.com/in/justin-zeng-3b7b47165/"
+			}
 		},
 		{
 			position: officerPositions.COPRESIDENT,
 			name: "Sachi Figliolini",
 			picture: _sachi,
-            linkedin: "http://www.linkedin.com/in/sachi-figliolini/"
+			socials: {
+				linkedin: "http://www.linkedin.com/in/sachi-figliolini/"
+			}
 		},
 		{
 			position: officerPositions.FINANCE,
 			name: "Phyllis Chen",
 			picture: _phyllis,
-			linkedin: "https://www.linkedin.com/in/phyllis-chen-profile"
+			socials: {
+				linkedin: "https://www.linkedin.com/in/phyllis-chen-profile"	
+			}
 		},
 		{
 			position: officerPositions.CREATIVE,
 			name: "Sarah Thomas",
 			picture: _sarah,
-            linkedin: "https://www.linkedin.com/in/sarahetthomas/",
-            website: "https://sarahthomas.me"
+			socials: {
+				linkedin: "https://www.linkedin.com/in/sarahetthomas/",
+				website: "https://sarahthomas.me"	
+			}
 		},
 		{
 			position: officerPositions.PUBLIC_RELATIONS,
 			name: "Brianna Pak",
             picture: _brianna,
-			linkedin: "https://www.linkedin.com/in/briannapak/",
+			socials: {
+				linkedin: "https://www.linkedin.com/in/briannapak/",	
+			}
 		},
 		{
 			position: officerPositions.OUTREACH,
 			name: "Sirena Akopyan",
 			picture: _sirena,
-			linkedin: "https://www.linkedin.com/in/sirena-akopyan-61862221a"
+			socials: {
+				linkedin: "https://www.linkedin.com/in/sirena-akopyan-61862221a"	
+			}
 		},
 		{
 			position: officerPositions.DIVERSITY,
 			name: "Pranav Shekar",
             picture: _pranav,
-            linkedin: "https://www.linkedin.com/in/pranav-shekar/",
-            website: "http://pranavshekar.com/"
+			socials: {
+				linkedin: "https://www.linkedin.com/in/pranav-shekar/",
+				website: "http://pranavshekar.com/"	
+			}
 		},
 		{
 			position: officerPositions.IT,
 			name: "Devina Tavathia",
 			picture: _devina,
-			linkedin: "https://www.linkedin.com/in/devina-tavathia-40722b167/",
-			website: "https://devinat.github.io/PersonalPortfolio/",
-			github: "https://github.com/DevinaT"
+			socials: {
+				linkedin: "https://www.linkedin.com/in/devina-tavathia-40722b167/",
+				website: "https://devinat.github.io/PersonalPortfolio/",
+				github: "https://github.com/DevinaT"	
+			}
 		},
 		{
 			position: officerPositions.ACADEMIC,
 			name: "Drew Favors",
             picture: _drew,
-			linkedin: "https://www.linkedin.com/in/drewfavors"
+			socials: {
+				linkedin: "https://www.linkedin.com/in/drewfavors"	
+			}
 		}
     ],
     [groupType.FYR]: [

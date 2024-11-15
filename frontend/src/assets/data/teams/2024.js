fa93af9d949f1d3ae37302bf60309416158bd983
mtran3 <102627861+mtran3@users.noreply.github.com>
2024-11-15 14:55:01 -0800

    Added 2024 exec

--- frontend/src/assets/data/teams/2024.js
+++ frontend/src/assets/data/teams/2024.js
@@ -6,57 +6,95 @@ import { webDevPositions, creativeCommitteePositions, diversityCommitteePosition
 // https://adobe.com/express/feature/image/resize
 // https://stackoverflow.com/questions/74502978/object-fit-cover-gives-pixelated-images-on-chrome
 
+/* 2024 officers images */
+import _2024_sheamin from "../officerPhotos/2024/officer_sheamin.jpg";
+import _2024_akash from "../officerPhotos/2024/officer_akash.png";
+import _2024_jr from "../officerPhotos/2024/officer_jr.jpg";
+import _2024_shirley from "../officerPhotos/2024/officer_shirley.png";
+import _2024_jada from "../officerPhotos/2024/officer_jada.png";
+import _2024_bright from "../officerPhotos/2024/officer_bright.png";
+import _2024_jonathan from "../officerPhotos/2024/officer_jonathan_.jpg";
+import _2024_michaela from "../officerPhotos/2024/officer_michaela.jpeg";
+import _2024_iris from "../officerPhotos/2024/officer_iris.png";
+
 export const team2024 = {
-        // 2024: {
-    //     [groupType.OFFICERS]: [
-    //         {
-    //             position: officerPositions.PRESIDENT,
-    //             name: "Sheamin Kim",
-    //             picture: "", //_2024_sheamin
-    //         },
-    //         {
-    //             position: officerPositions.VICE_PRESIDENT,
-    //             name: "Akash Shrestha",
-    //             picture: "", //_2024_akash
-    //         },
-    //         {
-    //             position: officerPositions.FINANCE,
-    //             name: "J.R. Lim",
-    //             picture: "",
-    //         },
-    //         {
-    //             position: officerPositions.CREATIVE,
-    //             name: "Shirley Yun",
-    //             picture: "", //_2024_shirley
-    //         },
-    //         {
-    //             position: officerPositions.PUBLIC_RELATIONS,
-    //             name: "Jada Nguyen",
-    //             picture: "", //_2024_jada
-    //         },
-    //         {
-    //             position: officerPositions.OUTREACH,
-    //             name: "Bright Hoang",
-    //             picture: "",
-    //         },
-    //         {
-    //             position: officerPositions.DIVERSITY,
-    //             name: "Jonathan Ortiz-Candelaria",
-    //             picture: "",
-    //         },
-    //         {
-    //             position: officerPositions.IT,
-    //             name: "Michaela Tran",
-    //             picture: "",
-    //         },
-    //         {
-    //             position: officerPositions.ACADEMIC,
-    //             name: "Iris Hamilton",
-    //             picture: "",
-    //         },
-    //     ],
-    //     [groupType.WEB_COMMITTEE]: [],
-    //     [groupType.CREATIVE_COMMITTEE]: [],
-    //     [groupType.FYR]: [],
-    // },
+        2024: {
+        [groupType.OFFICERS]: [
+            {
+                position: officerPositions.PRESIDENT,
+                name: "Sheamin Kim",
+                picture: _2024_sheamin,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/sheamink/", 
+                }
+            },
+            {
+                position: officerPositions.VICE_PRESIDENT,
+                name: "Akash Shrestha",
+                picture: _2024_akash,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/akashkshrestha/",
+                }
+            },
+            {
+                position: officerPositions.FINANCE,
+                name: "J.R. Lim",
+                picture: _2024_jr,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/jr-lim/",
+                }
+            },
+            {
+                position: officerPositions.CREATIVE,
+                name: "Shirley Yun",
+                picture: _2024_shirley,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/shirleyyun/",
+                }
+            },
+            {
+                position: officerPositions.PUBLIC_RELATIONS,
+                name: "Jada Nguyen",
+                picture: _2024_jada,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/jadanguyend/",
+                }
+            },
+            {
+                position: officerPositions.OUTREACH,
+                name: "Bright Hoang",
+                picture: _2024_bright,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/brighth/",
+                }
+            },
+            {
+                position: officerPositions.DIVERSITY,
+                name: "Jonathan Ortiz-Candelaria",
+                picture: _2024_jonathan,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/jonathan-hugh-ortiz-candelaria/",
+                }
+            },
+            {
+                position: officerPositions.IT,
+                name: "Michaela Tran",
+                picture: _2024_michaela,
+                socials: {
+                    linkedin: "www.linkedin.com/in/michaela-tran",
+                }
+            },
+            {
+                position: officerPositions.ACADEMIC,
+                name: "Iris Hamilton",
+                picture: _2024_iris,
+                socials: {
+                    linkedin: "https://www.linkedin.com/in/iris-ham/",
+                }
+            },
+        ],
+        [groupType.WEB_COMMITTEE]: [],
+        [groupType.CREATIVE_COMMITTEE]: [],
+        [groupType.FYR]: [],
+    },
 }

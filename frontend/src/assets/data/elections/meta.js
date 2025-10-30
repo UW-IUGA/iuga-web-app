fb89573436b77e33a6861f7695e75a1e4605c29a
Camilo <45835722+cmdharo@users.noreply.github.com>
2025-10-29 17:35:03 -0700

    Fix Elections page

--- frontend/src/assets/data/elections/meta.js
+++ frontend/src/assets/data/elections/meta.js
@@ -1,16 +1,16 @@
 export const electionsMeta = {
   2026: {
     active: false,
-    title: "Welcome to IUGA 2025–26 Elections!",
-    blurb: "Meet your 2025–26 candidates. Don’t forget to vote by Feb 28!",
-    voteLink: "https://forms.office.com/...",   // or null when closed
-    heroImg: "/assets/elections.png"
-  },
-  2025: {
-    active: false,
     title: "IUGA Elections",
     blurb: "There are no elections at this time. Check back later!",
     voteLink: null,
-    heroImg: "/assets/elections.png"
-  }
+    heroImg: "/assets/elections.png",
+  },
+  2025: {
+    active: true,
+    title: "Welcome to IUGA 2025–26 Elections!",
+    blurb: "Meet your 2025–26 candidates. Don’t forget to vote by Feb 28!",
+    voteLink: "https://forms.office.com/...", // or null if closed
+    heroImg: "/assets/elections.png",
+  },
 };

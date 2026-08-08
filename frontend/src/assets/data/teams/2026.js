1ccba77079a2e24e6508530461de312711c138e6
Yonie <164077864+Isaiahriveraa@users.noreply.github.com>
2026-08-07 20:55:36 -0700

    fix(import): dias photo

--- frontend/src/assets/data/teams/2026.js
+++ frontend/src/assets/data/teams/2026.js
@@ -13,7 +13,7 @@ import _2026_preeti from "../officerPhotos/2025/preeti.png";
 import _2026_yonie from "../officerPhotos/2026/yonie.png";
 import _2026_samantha from "../officerPhotos/2026/samantha.png";
 import _2026_nitya from "../officerPhotos/2026/nitya.png";
-import _2026_dia from "../officerPhotos/2026/dia.png";
+import _2026_dia from "../officerPhotos/2026/dia.jpg";
 
 export const team_2026 = {
   [groupType.OFFICERS]: [

978b2da8aac9ecdbd6ab197e09ac522c732e1fed
Yonie <164077864+Isaiahriveraa@users.noreply.github.com>
2026-08-11 21:32:32 -0700

    feat(api): add role catalog endpoints

--- backend/routes/api/v1/apiv1.js
+++ backend/routes/api/v1/apiv1.js
@@ -4,10 +4,12 @@ var router = express.Router();
 import usersRouter from "./controllers/user.js";
 import eventsRouter from "./controllers/events.js";
 import feedbackRouter from "./controllers/feedback.js";
+import rolesRouter from "./controllers/roles.js";
 
 router.use("/user", usersRouter);
 router.use("/events", eventsRouter);
 router.use("/feedback", feedbackRouter);
+router.use("/roles", rolesRouter);
 
 export default router;
 

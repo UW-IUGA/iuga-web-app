95396f1e371eed7c90924759359903b552523355
Yonie <164077864+Isaiahriveraa@users.noreply.github.com>
2026-08-20 21:37:50 -0700

    feat(navbar): refine mobile menu, centered logo, and hamburger a11y

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { NavLink, useLocation } from 'react-router-dom';
 import { useAuthContext } from "../context/AuthContext";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,15 +7,19 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar({ signIn, signOut }) {
     const [showMenu, setMenu] = useState(false);
+    const { pathname } = useLocation();
     const { isAuthenticated, user } = useAuthContext();
     const userGreeting = user?.uFirstName || user?.uDisplayName || user?.name || user?.email;
+    const closeMenu = () => setMenu(false);
+
+    useEffect(() => {
+        setMenu(false);
+    }, [pathname]);
 
     return (
         <nav>
-            <div className="nav-container">
+            <div className={`nav-container ${showMenu ? "nav-menu-open" : ""}`}>
                 <div className="nav-header">
-                    <NavLink to="/" className="nav-logo"><img src="/iuga-logo.png" alt="IUGA logo, links to home"></img></NavLink>
-                    <span></span>
                     <button
                         type="button"
                         className="nav-mobile-menu"
@@ -26,13 +30,15 @@ function Navbar({ signIn, signOut }) {
                     >
                         <FontAwesomeIcon icon={faBars} />
                     </button>
+                    <NavLink to="/" className="nav-logo" onClick={closeMenu}><img src="/iuga-logo.png" alt="IUGA logo, links to home"></img></NavLink>
+                    <span></span>
                 </div>
                 <div id="nav-items" className={`nav-items-wrapper ${showMenu ? "nav-show-items" : "nav-hide-items"}`}>
-                    <NavLink to="/" end>Home</NavLink>
-                    <NavLink to="/events">Events</NavLink>
-                    <NavLink to="/resources">Resources</NavLink>
+                    <NavLink to="/" end onClick={closeMenu}>Home</NavLink>
+                    <NavLink to="/events" onClick={closeMenu}>Events</NavLink>
+                    <NavLink to="/resources" onClick={closeMenu}>Resources</NavLink>
                     <span className="nav-shop">Shop</span>
-                    <NavLink to="/get-involved">Get Involved</NavLink>
+                    <NavLink to="/get-involved" onClick={closeMenu}>Get Involved</NavLink>
                     <span></span>
                     <div className="nav-auth">
                         {isAuthenticated ? (
